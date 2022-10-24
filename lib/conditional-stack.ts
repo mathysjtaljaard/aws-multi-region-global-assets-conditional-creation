d72ee0682d94e1c47d2f68ef78552d9833bc746c
Mathys J Taljaard <mathysjtaljaard@users.noreply.github.com>
2022-10-23 20:33:01 -0400

    Conditional Logic to for S3 Bucket creation

--- lib/conditional-stack.ts
+++ lib/conditional-stack.ts
@@ -1,7 +1,10 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_s3 as s3, CfnCondition, Fn } from 'aws-cdk-lib';
 
+
+const bucket_name = 'mathysglobalbucket';
 export class ConditionalStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -12,5 +15,20 @@ export class ConditionalStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'ConditionalQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    const s3_bucket = s3.Bucket.fromBucketAttributes(this, 's3_bucket_from_name', { bucketName: bucket_name })
+
+    const s3_bucket_exists = new CfnCondition(this, 'DoesS3BucketExist', {
+      expression: Fn.conditionEquals(s3_bucket.bucketName, '')
+    })
+    //
+
+    const the_bucket = new s3.Bucket(this, bucket_name, {
+      versioned: true,
+      bucketName: bucket_name,
+    })
+
+    const cfnBucket = the_bucket.node.defaultChild as s3.CfnBucket
+    cfnBucket.cfnOptions.condition = s3_bucket_exists
+
   }
 }
