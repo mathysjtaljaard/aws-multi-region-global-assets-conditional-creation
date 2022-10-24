fb4cfa08fe3f5e89978aa5f5cb9eac001f19fc18
Mathys J Taljaard <mathysjtaljaard@users.noreply.github.com>
2022-10-23 21:00:37 -0400

    Updates

--- README.md
+++ README.md
@@ -12,3 +12,53 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `cdk deploy`      deploy this stack to your default AWS account/region
 * `cdk diff`        compare deployed stack with current state
 * `cdk synth`       emits the synthesized CloudFormation template
+
+## Notes:
+
+### Access:
+
+With the new AWS CDK v2 there is additional `resources` created. ECR (Elastic Container Registery) and also System Manager Params (SSM).
+
+- Access Policy (Note: This is **`not`** the recommend `Least Privileged Access Model` but will get `cdk boostrap` to work )
+```json
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "s3:*",
+                "cloudformation:*",
+                "dynamodb:*",
+                "ssm:*Parameter*",
+                "iam:*",
+                "cloudwatch:*",
+                "ecr:*",
+            ],
+            "Resource": "*"
+        },
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": "sts:AssumeRole",
+            "Resource": "arn:aws:iam::*:role/cdk-*"
+        }
+    ]
+}
+```
+
+### Conditional Creation of S3 Bucket
+
+- [Inspartional Came From This Article](https://loige.co/create-resources-conditionally-with-cdk/)
+
+`TLDR;`
+
+- Perform an S3 Bucket from Name lookup or do an Function Import Value
+- - If bucket name is '' we will set the the S3 Bucket condition to true which relates to the creation of the bucket
+- - if the bucket name isn't blank then the S3 Bucket condition is false and the bucket isn't created. 
+- Now do a search again and use the given arn to setup your resources. 
+- When deploying cross region, your conditional logic will look for the `global s3 resource` prior to creating it
+
+---- 
+More to come
--- lib/conditional-stack.ts
+++ lib/conditional-stack.ts
@@ -1,7 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { aws_s3 as s3, CfnCondition, Fn } from 'aws-cdk-lib';
+import { aws_s3 as s3, CfnCondition, CfnOutput, Fn, RemovalPolicy } from 'aws-cdk-lib';
 
 
 const bucket_name = 'mathysglobalbucket';
@@ -11,24 +11,51 @@ export class ConditionalStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ConditionalQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-    const s3_bucket = s3.Bucket.fromBucketAttributes(this, 's3_bucket_from_name', { bucketName: bucket_name })
+    // Possibility 1
+    /*
+      1. Search for the bucket from the attributes such as arn or bucket name
+      2. Define the conditional logic 
+      3. Create the bucket conditionally
+      4. Search for the bucket from the attributes such as arn or bucket name
+    */
+    let searchForS3BucketGivenName = s3.Bucket.fromBucketAttributes(this, 's3_bucket_from_name', { bucketName: bucket_name })
 
-    const s3_bucket_exists = new CfnCondition(this, 'DoesS3BucketExist', {
-      expression: Fn.conditionEquals(s3_bucket.bucketName, '')
+    const doesBucketExists = new CfnCondition(this, 'DoesS3BucketExist', {
+      expression: Fn.conditionEquals(searchForS3BucketGivenName.bucketName, '')
     })
-    //
 
-    const the_bucket = new s3.Bucket(this, bucket_name, {
+    // Possiblity 2
+    /*
+      1. Import the bucket arn using Fn.importValue()
+      2. Define the conditional logic 
+      3. Create the bucket conditionally
+      4. Search for the bucket from the attributes such as arn or bucket name
+    */
+    /*
+     const valueOfBucketOutputArnValue = Fn.importValue('mathysglobalbucket)
+     const doesBucketExists = new CfnCondition(this, 'DoesS3BucketExist', {
+       expression: Fn.conditionEquals(valueOfBucketOutputArnValue, '')
+     })
+    */
+    const creatingBucketConditionally = new s3.Bucket(this, bucket_name, {
       versioned: true,
       bucketName: bucket_name,
+      removalPolicy: RemovalPolicy.DESTROY
     })
 
-    const cfnBucket = the_bucket.node.defaultChild as s3.CfnBucket
-    cfnBucket.cfnOptions.condition = s3_bucket_exists
+    const cfnBucket = creatingBucketConditionally.node.defaultChild as s3.CfnBucket
+    cfnBucket.cfnOptions.condition = doesBucketExists
+
+    const createdBucket = s3.Bucket.fromBucketAttributes(this, 's3_bucket_from_name', { bucketName: bucket_name })
+
+    // use this variable going forward for resources needing access to the S3 Bucket
+    // outputs
+    // Possible to use `Fn.importValue(exportName)` 
+
+    new CfnOutput(this, 'S3BucketOutput', {
+      value: createdBucket.bucketArn,
+      exportName: 'mathysglobalbucket'
+    })
 
   }
 }
